6e46f3c0914ce21f3b21d0c1a98d7347d3868371
Anthony McLamb <adylanmclamb@yahoo.com>
2024-01-26 14:41:56 -0500

    Update some stuff

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,9 @@
-import { useState } from "react";
 import "./App.css";
-import OpenaiChat from "./feature/openai/openai-chat/OpenaiChat";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
-      <OpenaiChat />
+      <p>TEST</p>
     </>
   );
 }
