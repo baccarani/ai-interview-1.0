aab62ad8e1692aac11c11ca88118d3dadbb8a9c5
Anthony McLamb <adylanmclamb@yahoo.com>
2024-02-01 13:32:28 -0500

    Update home page

--- src/pages/home/HomePage.tsx
+++ src/pages/home/HomePage.tsx
@@ -1,6 +1,6 @@
 import { Card } from "@/components/ui/card";
 import Interview from "@/features/interview/Interview";
-import Recorder from "@/features/recorder/Recorder";
+// import Recorder from "@/features/recorder/Recorder";
 import Layout from "@/layout/Layout";
 import Container from "@/layout/container/Container";
 
@@ -9,7 +9,7 @@ const HomePage = () => {
     <Layout>
       <Container className="grid grid-cols-1 gap-5 py-5">
         <Card className="p-5">
-          <Recorder fileName="random" />
+          {/* <Recorder fileName="random" /> */}
           <h1 className="text-2xl font-semibold">AI Interview</h1>
           <p className="text-slate-500">
             Hone your interview skills practicing with AI
