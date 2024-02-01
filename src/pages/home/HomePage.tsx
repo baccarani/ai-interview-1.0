d20b0352e17b274bdeebcb6b929828e6533ea05d
Anthony McLamb <adylanmclamb@yahoo.com>
2024-02-01 13:30:06 -0500

    update home page

--- src/pages/home/HomePage.tsx
+++ src/pages/home/HomePage.tsx
@@ -1,14 +1,12 @@
 import { Card } from "@/components/ui/card";
 import Interview from "@/features/interview/Interview";
 import Recorder from "@/features/recorder/Recorder";
-import { ModeToggle } from "@/features/theme/ModeToggle";
 import Layout from "@/layout/Layout";
 import Container from "@/layout/container/Container";
 
 const HomePage = () => {
   return (
     <Layout>
-      <ModeToggle />
       <Container className="grid grid-cols-1 gap-5 py-5">
         <Card className="p-5">
           <Recorder fileName="random" />
