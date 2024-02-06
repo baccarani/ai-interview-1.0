cbebc35e3c1029ba9897387710bba71b1a79034e
Baccarani Pham <baccaranip@gmail.com>
2024-02-06 14:46:55 -0500

    Added call to OpenAI for audio-to-text

--- src/features/recorder/Recorder.tsx
+++ src/features/recorder/Recorder.tsx
@@ -1,14 +1,15 @@
 import { Button } from '../../components/ui/button';
-import { invokeSaveAsDialog } from 'recordrtc';
+// import { invokeSaveAsDialog } from 'recordrtc';
 import { useRecorderPermission } from './useRecorderPermission';
+import axios from 'axios'
 
 interface RecorderProps {
     fileName: string;
 }
 
 const Recorder = ({ fileName }: RecorderProps) => {
-
     const recorder = useRecorderPermission('audio')
+    const API_URL = 'https://api.openai.com/v1/audio/transcriptions'
 
     const startRecording = async () => {
         recorder.startRecording()
@@ -17,7 +18,26 @@ const Recorder = ({ fileName }: RecorderProps) => {
     const stopRecording = async () => {
         await recorder.stopRecording()
         let blob = await recorder.getBlob()
-        invokeSaveAsDialog(blob, `${fileName}.webm`)
+        // invokeSaveAsDialog(blob, `${fileName}.mp3`)
+
+        let formData = new FormData();
+        formData.append("file", blob, `${fileName}.mp3`);
+        formData.append("model", "whisper-1");
+
+        let response = await postData(API_URL, formData);
+        console.log(response);
+    }
+
+    const axiosInstance = axios.create({
+        baseURL: API_URL,
+        headers: {
+            Authorization: `Bearer ${import.meta.env.VITE_OPEN_AI_API_KEY}`,
+            'Content-Type': 'multipart/form-data',
+        },
+    });
+
+    const postData = async (url: string, data: any) => {
+        return await axiosInstance.post(url, data)
     }
 
     return (
