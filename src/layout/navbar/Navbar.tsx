ab036179e7544341b5e7b2f5f36a81b5929fdc61
Baccarani Pham <baccaranip@gmail.com>
2024-02-07 16:49:09 -0500

    Updated navbar spacing to align with the page content

--- src/layout/navbar/Navbar.tsx
+++ src/layout/navbar/Navbar.tsx
@@ -5,7 +5,7 @@ import { Link } from "react-router-dom";
 const Navbar = () => {
   return (
     <nav className="border-b">
-      <Container className="flex justify-between items-center p-3 relative">
+      <Container className="flex justify-between items-center py-3 relative">
         <Link to="/">
           <h1 className="font-semibold text-lg">Ai Interviews</h1>
         </Link>
