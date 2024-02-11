19aa466b1f5436e46138d589bdc2d66e59979d1c
Baccarani Pham <baccaranip@gmail.com>
2024-02-11 18:26:36 -0500

    Stopping audio from playing when the user leave the interview page and updated secondary buton coloring

--- src/features/interview/Interview.tsx
+++ src/features/interview/Interview.tsx
@@ -4,7 +4,7 @@ import { useState } from "react";
 import { ChatResponse, openaiService } from "@/services/OpenaiService";
 import Chat from "@/components/chat/Chat";
 import InterviewResponse from "./interview-response/InterviewResponse";
-import axios from "axios";
+import axios, { CancelTokenSource } from "axios";
 import { OpenaiModelTypes } from "@/services/OpenaiModelTypes";
 
 type Props = {
@@ -16,9 +16,11 @@ const Interview = ({ role }: Props) => {
   const [isLoading, setIsLoading] = useState<boolean>(false);
   const [isProcessingAudio, setIsProcessingAudio] = useState(true);
   const audioRef = useRef<HTMLAudioElement | null>(null);
+  const isMountedRef = useRef(true);
 
   useEffect(() => {
     let ignore = false;
+    const source = axios.CancelToken.source();
     (async () => {
       setIsLoading(true);
       const initialInterview = await openaiService.startQuestions(role);
@@ -27,34 +29,43 @@ const Interview = ({ role }: Props) => {
           model: "tts-1-hd",
           voice: "alloy",
           input: initialInterview[0].content,
-        }, 'blob').then((audioBlob) => {
+        }, 'blob', source.token).then((audioBlob) => {
           const audio = new Audio(URL.createObjectURL(audioBlob.data));
           audioRef.current = audio;
           setMessages(initialInterview);
           setIsLoading(false);
           audio.play();
           audio.onended = () => setIsProcessingAudio(false);
+        }).catch((thrown) => {
+          if (axios.isCancel(thrown)) {
+            console.log('Request canceled', thrown.message);
+          } else {
+            console.log('Error', thrown);
+          }
         });
       }
     })();
     return () => {
       ignore = true;
+      source.cancel('Operation canceled by the user.');
       if (audioRef.current) {
         audioRef.current.pause();
         audioRef.current.currentTime = 0;
         audioRef.current.src = "";
+        isMountedRef.current = false;
       }
     };
   }, []);
 
   const addMessage = (newMessage: ChatResponse) => {
+    const source = axios.CancelToken.source();
     setIsLoading(true);
     setMessages((prevMessages) => [...prevMessages, newMessage]);
     const chatFormData = {
       messages: [...messages, newMessage],
       model: OpenaiModelTypes.GPT_3_TURBO,
     };
-    postData("https://api.openai.com/v1/chat/completions", chatFormData).then((chatResponse) => {
+    postData("https://api.openai.com/v1/chat/completions", chatFormData, 'json', source.token).then((chatResponse) => {
       const assistantMessage = {
         content: chatResponse.data.choices[0].message.content,
         role: "assistant",
@@ -63,17 +74,33 @@ const Interview = ({ role }: Props) => {
         model: "tts-1-hd",
         voice: "alloy",
         input: assistantMessage.content,
-      }, 'blob').then((audioBlob) => {
-        const audio = new Audio(URL.createObjectURL(audioBlob.data));
-        audioRef.current = audio;
-        setMessages((prevMessages) => [
-          ...prevMessages,
-          assistantMessage,
-        ]);
-        setIsLoading(false);
-        audio.play();
-        audio.onended = () => setIsProcessingAudio(false);
+      }, 'blob', source.token).then((audioBlob) => {
+        if (isMountedRef.current) {
+          const audio = new Audio(URL.createObjectURL(audioBlob.data));
+          audioRef.current = audio;
+          setMessages((prevMessages) => [
+            ...prevMessages,
+            assistantMessage,
+          ]);
+          setIsLoading(false);
+          audio.play();
+          audio.onended = () => {
+            if (isMountedRef.current) {
+              setIsProcessingAudio(false);
+            }
+          };
+        }
+      }).catch((thrown) => {
+        if (axios.isCancel(thrown)) {
+          console.log('Request canceled', thrown.message);
+        } else {
+          console.log('Error', thrown);}
       });
+    }).catch((thrown) => {
+      if (axios.isCancel(thrown)) {
+        console.log('Request canceled', thrown.message);
+      } else {
+        console.log('Error', thrown);}
     });
   };
 
@@ -84,8 +111,8 @@ const Interview = ({ role }: Props) => {
     },
   });
 
-  const postData = async (url: string, data: any, responseType: 'json' | 'blob' = 'json') => {
-    return await axiosInstance.post(url, data, { responseType });
+  const postData = async (url: string, data: any, responseType: 'json' | 'blob' = 'json', cancelToken: CancelTokenSource['token']) => {
+    return await axiosInstance.post(url, data, { responseType, cancelToken });
   };
 
   return (
